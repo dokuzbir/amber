7ca6f52a3162ca4934804e64307c79760618f8b0
Faustino Aguilar <faustinoaq@gmail.com>
2017-12-26 15:11:40 -0500

    Fix spec

--- spec/amber/router/pipe/reload_spec.cr
+++ spec/amber/router/pipe/reload_spec.cr
@@ -3,22 +3,21 @@ require "../../../../spec_helper"
 module Amber
   module Pipe
     describe Reload do
+      pipeline = Pipeline.new
+      request = HTTP::Request.new("GET", "/reload")
+
+      Amber::Server.router.draw :web do
+        get "/reload", HelloController, :world
+      end
+
+      pipeline.build :web do
+        plug Amber::Pipe::Reload.new
+      end
+
+      pipeline.prepare_pipelines
+
       it "client should contain injected code" do
-        reload = Reload.new
-        pipeline = Pipeline.new
-        request = HTTP::Request.new("GET", "/reload")
-
-        Amber::Server.router.draw :web do
-          get "/reload", HelloController, :index
-        end
-
-        pipeline.build :web do
-          plug Amber::Pipe::Reload.new
-        end
-
-        reload.next = ->(context : HTTP::Server::Context) { "Hello World!" }
-        response = create_request_and_return_io(reload, request)
-
+        response = create_request_and_return_io(pipeline, request)
         response.body.should contain "Code injected by Amber Framework"
       end
     end
